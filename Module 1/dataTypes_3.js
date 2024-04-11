6e2c4dba4b56ec4123ebcc29734e9ab2f5727c91
Rendy1403 <buxyxol2000@gmail.com>
2024-04-11 11:13:42 +0300

    Added task 3 of module 1

---
+++ Module 1/dataTypes_3.js
@@ -0,0 +1,26 @@
+function compareObjects(obj1, obj2) {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (let key of keys1) {
+        if (!obj2.hasOwnProperty(key)) {
+            return false;
+        }
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+const obj1 = { a: 1, b: 2, c: 3 };
+const obj2 = { a: 1, b: 2, c: 3 };
+const obj3 = { a: 1, b: 2, c: 4 };
+
+console.log(compareObjects(obj1, obj2));
+console.log(compareObjects(obj1, obj3));
