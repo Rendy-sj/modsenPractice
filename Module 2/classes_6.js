59c53896789349e928a085dc9e72af546c791070
Rendy1403 <buxyxol2000@gmail.com>
2024-04-17 11:00:26 +0300

    Added task 6 of module 2

---
+++ Module 2/classes_6.js
@@ -0,0 +1,21 @@
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    calculateArea() {
+        return this.width * this.height;
+    }
+
+    calculatePerimeter() {
+        return 2 * (this.width + this.height);
+    }
+}
+
+const rectangle = new Rectangle(5, 8);
+
+const area = rectangle.calculateArea();
+const perimeter = rectangle.calculatePerimeter();
+
+
