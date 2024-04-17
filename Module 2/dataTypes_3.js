ac855470fb3db92f273522f477814b7d452e15a9
Rendy1403 <buxyxol2000@gmail.com>
2024-04-17 10:50:48 +0300

    Added task 3 of module 2

---
+++ Module 2/dataTypes_3.js
@@ -0,0 +1,9 @@
+function printLastCharacter(str) {
+    if (str.length === 0) {
+        console.log("String is empty");
+    } else {
+        console.log("Last character:", str.charAt(str.length - 1));
+    }
+}
+
+
