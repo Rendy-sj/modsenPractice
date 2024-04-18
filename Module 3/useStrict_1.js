1e79c33f0d6126ca13f30b6c7d08789d9fbfcb23
Rendy-sj <kirillmironoff5@gmail.com>
2024-04-18 08:07:43 +0300

    Added task 1 of module 3

---
+++ Module 3/useStrict_1.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function countVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+
